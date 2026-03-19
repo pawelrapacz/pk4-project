df32c3d5f9371c305ce9954c897c8e26d915c820
Paweł Rapacz <apacz_paw@wp.pl>
2026-03-19 09:52:54 +0100

    Add Application

---
+++ src/Application.cpp
@@ -0,0 +1,42 @@
+module;
+
+#include <raylib.h>
+#include <iostream>
+
+module Application;
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+
+Application::Application(const ApplicationSpecs& specs)
+    : _specs(specs) { }
+
+void Application::Run() {
+    std::clog << _specs.width <<std::endl << _specs.height << std::endl;
+    InitWindow(_specs.width * _specs.scale, _specs.height * _specs.scale, _specs.title);
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()) {
+        UpdateDrawFrame();
+    }
+#endif
+
+    CloseWindow();
+}
+
+
+void Application::UpdateDrawFrame() {
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    DrawText("Battleships!", 500, 300, 40, LIGHTGRAY);
+
+    EndDrawing();
+}
